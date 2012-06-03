5e02a5f98451702bb2ac071af378bf1710495e90
Joseph McCormick <esmevane@gmail.com>
2012-06-03 13:53:23 -0500

    Updated to include RequireJS for dependency management.

--- scripts/src/application.js
+++ scripts/src/application.js
@@ -1,1 +1,13 @@
-window.BaselineApp = {};
+require(
+  [
+    '../lib/jquery',
+    '../lib/underscore',
+    '../lib/backbone',
+    '../lib/backbone-local-storage',
+    '../lib/chosen',
+    '../lib/bootstrap'
+  ],
+  function() {
+    window.BaselineApp = {};
+  }
+);
